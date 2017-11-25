8421b5c1396beee004f811f6706bf36582eaed6f
Omar Farooq Ilyas <omar1490@gmail.com>
2017-11-25 16:26:05 +0100

    Endret språket for kalender rammeverket til norsk

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,5 +1,16 @@
 var SRMApp = angular
     .module('simplerotamanager', ['mwl.calendar', 'ngAnimate', 'ui.bootstrap'])
     .config(['calendarConfig', function (calendarConfig) {
+        calendarConfig.dateFormatter = 'moment';
         calendarConfig.templates.calendarSlideBox = '/templates/calendarSlidebox.html';
+        calendarConfig.i18nStrings.weekNumber = 'Uke {week}';
+        calendarConfig.allDateFormats.moment.date.hour = 'HH:mm';
+
+        moment.locale('nb', {
+            week: {
+                dow: 1
+            }
+        });
+        moment.locale('nb');
+
     }]);
